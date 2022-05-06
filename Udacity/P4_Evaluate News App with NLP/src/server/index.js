95d0c168c0fa03818d38980f3fc718833652a1f2
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-06 17:19:53 +0900

    L4 before installing service pack

--- Udacity/P4_Evaluate News App with NLP/src/server/index.js
+++ Udacity/P4_Evaluate News App with NLP/src/server/index.js
@@ -10,12 +10,13 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8085, function () {
+    console.log('Example app listening on port 8085!')
+   })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
