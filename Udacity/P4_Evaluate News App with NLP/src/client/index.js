c07655c4c04954c5173d07b62b522312dfc23e3c
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-14 01:01:19 +0900

    Add setEvent

--- Udacity/P4_Evaluate News App with NLP/src/client/index.js
+++ Udacity/P4_Evaluate News App with NLP/src/client/index.js
@@ -1,11 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { setEvent } from './js/setEvent'
 
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+// import { setEvent } from '../server/memo'
 
 export {
     checkForName,
@@ -15,3 +17,4 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+setEvent();
