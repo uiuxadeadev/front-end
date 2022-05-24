083f28c01a3f32ce4b9f1e9e49086f9d01ec4158
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-24 19:51:40 +0900

    P5 setting

---
+++ Udacity/P5_Travel App/__test__/postURL.test.js
@@ -0,0 +1,7 @@
+// Import the js file to test
+import { postURL } from "../src/client/js/postURL"
+
+describe("Testing the submit functionality", () => {
+    test("Testing the postURL() function", () => {
+    expect(postURL).toBeDefined();
+})});
