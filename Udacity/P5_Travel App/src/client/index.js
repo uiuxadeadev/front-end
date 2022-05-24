a936d2833ef6965a5f8d3a5dd6ebbeedd5512697
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-24 21:40:35 +0900

    set webpack

---
+++ Udacity/P5_Travel App/src/client/index.js
@@ -0,0 +1,19 @@
+import { setSubmit } from './js/setSubmit'
+import { setSubmitFromHtml } from './js/setSubmitFromHtml'
+import { postURL } from './js/postURL'
+import { validateURL } from './js/validateURL'
+import { updateUI } from './js/updateUI'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export {
+    setSubmitFromHtml,
+    postURL,
+    validateURL,
+    updateUI
+}
+// setSubmit();
