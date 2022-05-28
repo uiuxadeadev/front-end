0d062cad08feacfd7c55dd3ec05922e356895620
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-28 20:41:13 +0900

    add geoData process

---
+++ Udacity/P5_Travel App/src/server/getGeoData.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+export async function getCoordinates(destination){
+    //Call The Geonames Api to get the lat & long data of the destination.
+    const baseURl = 'http://api.geonames.org/searchJSON?'
+    const username = process.env.GEONAMES_USERNAME;
+
+    try{
+        const url = `${baseURl}q=${destination}&maxRows=1&username=${username}`;
+        const destinationData = await fetch(url);
+        const jsonData = await destinationData.json();
+        const response = jsonData.geonames[0];
+
+        const resultGeoData = response.data.geonames;
+        if (resultGeoData == null) {
+            return res.status(404).json({ Validation: "please input invalid city name" });
+        }
+        return resultGeoData
+        // if(geoData.length != 0){
+        //     return{
+        //         lat: geoData[0].lat,
+        //         lon: geoData[0].lng
+        //     };
+        // }else{
+        //     let code = (typeof response.response === 'undefined') ? 400 : response.response.status;
+        //     let message = response.data.error || 'Invalid city. Please provide a valid city name!';
+        //     return {
+        //         code: code ,
+        //         message: message
+        //     }
+        // }
+
+    }catch(err){
+        throw new Error(`Error has occured:${err}`);
+    }
+};
--- Udacity/P5_Travel App/src/server/server.js
+++ Udacity/P5_Travel App/src/server/server.js
@@ -17,30 +17,39 @@ console.log(__dirname)
 
 projectData = {};
 
-//API https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/examples
-const apiKey = process.env.API_KEY;
-const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
-console.log(`Your API key is ${process.env.API_KEY}`);
+//API http://www.geonames.org/export/web-services.html
+//API https://www.weatherbit.io/account/dashboard
+//API https://pixabay.com/api/docs/#api_search_images
 
 // POST Route
-app.post('/addData', async function (req, res){
-    const inputURL = req.body.inputText;
-    const url = `${baseUrl}?key=${apiKey}&url=${inputURL}&lang=en`;
-    const response = await fetch(url);
-    try {
-        const newData = await response.json();
-        // console.log(JSON.stringify(newData));
-        let nlpEntry = {
-            model: newData.model,
-            score_tag: newData.score_tag,
-            agreement: newData.agreement,
-            subjectivity: newData.subjectivity,
-            confidence: newData.confidence,
-            irony: newData.irony
-        };
-        projectData = nlpEntry;
-        console.log(projectData);
-        res.send(projectData);
+app.post('/addTrip', async (req, res) => {
+    const destination = req.body.city;
+    const resultGeoData = await getGeoData(destination);
+
+
+
+
+
+    try{
+
+
+    // const inputURL = req.body.inputText;
+    // const url = `${baseUrl}?key=${apiKey}&url=${inputURL}&lang=en`;
+    // const response = await fetch(url);
+    // try {
+    //     const newData = await response.json();
+    //     // console.log(JSON.stringify(newData));
+    //     let nlpEntry = {
+    //         model: newData.model,
+    //         score_tag: newData.score_tag,
+    //         agreement: newData.agreement,
+    //         subjectivity: newData.subjectivity,
+    //         confidence: newData.confidence,
+    //         irony: newData.irony
+    //     };
+    //     projectData = nlpEntry;
+    //     console.log(projectData);
+    //     res.send(projectData);
     } catch (error) {
         console.log("error", error);
         }
@@ -48,14 +57,15 @@ app.post('/addData', async function (req, res){
 
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+const port = 8081;
+app.listen(port, () =>
+    console.log('Example app listening on ${port}!')
+);
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get('/test', (req, res) => {
+    res.send(mockAPIResponse);
+});
 
-app.get('/apiData', (req, res)=>{
+app.get('/apiData', (req, res) => {
     res.send(apiData);
 });
