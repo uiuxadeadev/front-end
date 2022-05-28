23c29f2b1188c4564d5506a0f9283c0125b2dcdb
uiuxatai2022 <sun.a.tai2022@gmail.com>
2022-05-28 14:51:04 +0900

    Splitting cors file

--- Udacity/P5_Travel App/src/server/server.js
+++ Udacity/P5_Travel App/src/server/server.js
@@ -2,34 +2,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const path = require('path')
-const express = require('./express')
-const mockAPIResponse = require('./mockAPI.js')
+const path = require('path');
+const express = require('./express');
+const cors = require('./cors');
 const fetch = require('node-fetch');
-const cors = require('cors')
-
-// cors is needed to allow requests from another server
-app.use(cors({
-    origin: 'http://localhost:8080'
-}))
-
-console.log(__dirname)
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
-
-
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
 
 projectData = {};
-app.get('/apiData', (req, res)=>{
-    res.send(apiData);
-});
 
 //API https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/examples
 const apiKey = process.env.API_KEY;
@@ -37,7 +15,7 @@ const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
 console.log(`Your API key is ${process.env.API_KEY}`);
 
 // POST Route
-app.post('/addData', async function (req, res){
+express.post('/addData', async function (req, res){
     const inputURL = req.body.inputText;
     const url = `${baseUrl}?key=${apiKey}&url=${inputURL}&lang=en`;
     const response = await fetch(url);
@@ -58,4 +36,18 @@ app.post('/addData', async function (req, res){
     } catch (error) {
         console.log("error", error);
         }
+});
+
+
+// designates what port the app will listen to for incoming requests
+express.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+express.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+express.get('/apiData', (req, res)=>{
+    res.send(apiData);
 });
